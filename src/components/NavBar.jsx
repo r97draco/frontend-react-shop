2caabf3f2d3536eb2ffdb72dc70e09c2728ee307
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-03-17 21:07:04 -0600

    removed warnings

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,15 +8,15 @@ const NavBar = () => {
   return (
     <header className="text-gray-600 body-font">
       <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
-        <a className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
+        <button className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
           <AiFillShopping className="text-5xl" />
           <span className="ml-3 text-xl">Shop</span>
-        </a>
+        </button>
         <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
-          <a className="mr-5 hover:text-gray-900">Home</a>
-          <a className="mr-5 hover:text-gray-900">Gallery</a>
-          <a className="mr-5 hover:text-gray-900">Products</a>
-          <a className="mr-5 hover:text-gray-900">Find US</a>
+          <button className="mr-5 hover:text-gray-900">Home</button>
+          <button className="mr-5 hover:text-gray-900">Gallery</button>
+          <button className="mr-5 hover:text-gray-900">Products</button>
+          <button className="mr-5 hover:text-gray-900">Find US</button>
         </nav>
         <button
           className="inline-flex items-center px-3 py-1 mt-4 space-x-2 text-base bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0"
